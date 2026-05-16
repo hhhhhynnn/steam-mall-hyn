ed00b96a221534b8dc353f01215f6e9ea7626dd5
Ashen One <846212623@qq.com>
2026-05-16 19:39:30 +0800

    Update steam_mall.sql

--- steam-mall-backend/target/classes/db/steam_mall.sql
+++ steam-mall-backend/target/classes/db/steam_mall.sql
@@ -35,13 +35,6 @@ CREATE TABLE `activation_code`  (
   UNIQUE INDEX `UK_8ajx3tnkbhwdupdh80igwbvol`(`code`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of activation_code
--- ----------------------------
-INSERT INTO `activation_code` VALUES (1, 'CFXR-PBEB-S23Q-UHV7', '2026-04-09 20:32:53.093803', '2036-04-09 20:32:53.093803', 5, '赛博朋克 2077', 1, 0, '2026-04-09 20:32:53.093803');
-INSERT INTO `activation_code` VALUES (4, 'T7E3-RXKQ-EAEQ-Q3ED', '2026-04-18 22:41:51.916082', '2036-04-18 22:41:51.916082', 9, '巫师 3：狂猎', 6, 0, '2026-04-18 22:41:51.916082');
-INSERT INTO `activation_code` VALUES (5, 'FTHJ-W2YJ-YYM2-QU3S', '2026-04-18 23:07:15.470786', '2036-04-18 23:07:15.470786', 15, 'Apex 英雄', 7, 0, '2026-04-18 23:07:15.470786');
-INSERT INTO `activation_code` VALUES (11, 'B86F-XG6P-GYWM-TXZ7', '2026-04-20 00:29:46.624955', '2036-04-20 00:29:46.624955', 4, '艾尔登法环', 13, 0, '2026-04-20 00:29:46.624955');
 
 -- ----------------------------
 -- Table structure for carousel
@@ -108,19 +101,6 @@ CREATE TABLE `game`  (
   FULLTEXT INDEX `idx_search`(`name`, `description`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 20 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '游戏表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of game
--- ----------------------------
-INSERT INTO `game` VALUES (4, '艾尔登法环', '', '艾尔登法环是以正统黑暗奇幻世界为舞台的动作RPG游戏。 走进辽阔的场景与地下迷宫探索未知，挑战困难重重的险境，享受克服困境时的成就感吧。 不仅如此，登场角色之间的利害关系谱成的群像剧，更是不容错过。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/9d4856db-45cb-4c33-bc97-e063a34a740e.jpg', '', 298.00, 0.00, 298.00, 'FromSoftware', 'Bandai Namco Entertainment', '2022-02-26', '角色扮演,冒险,动作', '', NULL, '', 1, 16, 3, 0, 0, '2026-03-21 12:23:41', '2026-04-22 22:38:42', NULL);
-INSERT INTO `game` VALUES (5, '赛博朋克 2077', '赛博朋克 2077 是一款开放世界动作冒险 RPG 游戏。故事发生在夜之城，这是一座五光十色的大都会，人们痴迷于力量、华丽和身体改造。', '开放世界动作冒险 RPG', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/297cf394-edf0-4342-bdd4-da00ab4082e3.jpg', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/d7bfd2c6-8763-4b65-9ed3-43be89a76cf0.jpg', 298.00, 65.00, 104.30, 'CD Projekt Red', 'CD Projekt', '2020-12-10', '角色扮演,冒险,动作', NULL, NULL, NULL, 1, 15, 3, 0, 0, '2026-04-01 22:45:48', '2026-04-23 00:00:18', NULL);
-INSERT INTO `game` VALUES (6, '星露谷物语', '你继承了爷爷在星露谷的农场。你带着二手工具和一袋种子开始了你的新生活。你的任务是让这片土地重新焕发生机。', '农场模拟 RPG', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/f50873bb-704d-4daa-96ee-b3d123329692.jpg', '/images/stardew-banner.jpg', 48.00, 50.00, 24.00, 'ConcernedApe', 'ConcernedApe', '2016-02-26', '模拟,角色扮演', NULL, NULL, NULL, 1, 5, 1, 1, 0, '2026-04-01 22:45:48', '2026-04-19 22:53:25', NULL);
-INSERT INTO `game` VALUES (8, '只狼：影逝二度', '在《Sekiro: Shadows Die Twice (只狼：影逝二度)》中你是“独臂之狼”，一个名誉不再、伤痕累累的忍者，一个从死亡边缘捡回一命的战士。你效忠守护继承古老血统的年轻皇子，与危险的苇名一族以及众多凶恶之徒为敌。年轻的皇子被抓走后，为挽回荣誉，你将不畏死亡，踏上危机四伏的征程。\n\n探索生死冲突不断的16世纪后期，感受残酷的日本战国时代，在黑暗、扭曲的世界，与威胁生命的敌人对峙。活用义手装备各种致命武器，大显忍者身手，在血腥对抗中潜行、上下穿梭，与敌人正面激烈交锋。\n\n复仇雪耻。夺回荣誉。巧妙杀敌。', '进入由打造了《黑暗之魂》系列的知名开发商FromSoftware倾力制作的全新冒险，用智慧和力量斩开复仇之路。 决死复仇，夺回荣誉，智杀强敌。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/9c435e21-6736-4d40-abf3-8dc7163867eb.jpg', '/images/sekiro-banner.jpg', 198.00, 30.00, 138.60, 'FromSoftware', 'Activision', '2019-03-22', '动作,角色扮演,冒险', NULL, NULL, NULL, 1, 4, 1, 0, 0, '2026-04-01 22:45:48', '2026-04-20 00:38:15', NULL);
-INSERT INTO `game` VALUES (9, '巫师 3：狂猎', '作为巫师，一位猎杀怪物的雇佣法师，你要在道德和政治交织的复杂世界中寻找预言之子。', '您是利维亚的杰洛特，收钱办事的怪物杀手。您可以在眼前这片怪物横行、饱受战火摧残的土地上尽情探索。您手上的委托？追踪预言之子——希里，一件足以改变世界面貌的活生生的武器。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/83450d3e-31d0-47bb-a828-1c3c5b02e162.jpg', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/0f5dc795-8670-4321-90e7-b39cc319cc9d.jpg', 158.00, 75.00, 39.50, 'CD Projekt Red', 'CD Projekt', '2015-05-19', '角色扮演,冒险', NULL, NULL, NULL, 1, 2, 1, 0, 0, '2026-04-01 22:45:48', '2026-04-19 22:02:52', NULL);
-INSERT INTO `game` VALUES (10, '荒野大镖客：救赎 2', '美国，1899 年。亚瑟·摩根和范德林德帮众是一群逃亡在外的亡命之徒。联邦侦探和全国顶尖的赏金猎人在他们的身后穷追不舍，亚瑟一行人必须在广袤蛮荒的美国腹地上四处劫掠、挣扎求生。而帮派内部的矛盾分化日渐加深，摆在亚瑟面前的将是他无法避免的抉择：究竟是选择自己的理想，还是选择效忠于抚养了自己的帮派。\n\nRed Dead Redemption 2 已荣获超过 175 项年度游戏奖项且获得超过 250 个满分评价，游戏以 19 世纪的最后岁月为背景，述说一个关于荣誉与忠诚的史诗故事。\n\nRed Dead Redemption 2 还包含 Red Dead 在线模式，一款以 Red Dead Redemption 2 生活世界为背景的多人游戏体验，玩家可以独自骑行或组建团队，酿造私酒，与执法人员、不法帮派和凶猛的野生动物战斗，在美洲边境开辟自己的道路。', '亚瑟·摩根和范德林德帮众是一群逃亡在外的亡命之徒。联邦侦探和赏金猎人在他们的身后穷追不舍，亚瑟一行人必须在广袤蛮荒的腹地上四处劫掠、挣扎求生。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/375fdfe4-5abf-4d7e-87ae-2a2664e693a5.jpg', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/a95839a9-a255-4502-b9a3-f32618d591f1.jpg', 279.00, 50.00, 139.50, 'Rockstar Games', 'Rockstar Games', '2019-12-05', '动作,角色扮演', NULL, NULL, NULL, 1, 3, 0, 0, 0, '2026-04-01 22:45:48', '2026-04-19 22:23:57', NULL);
-INSERT INTO `game` VALUES (15, 'Apex 英雄', 'Apex Legends 是一款免费的大逃杀射击游戏，拥有传奇角色、团队玩法和创新的重生系统。', '免费大逃杀射击游戏', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/357d8a08-728e-4793-a062-57fc94ccf143.jpg', '/images/apex-banner.jpg', 0.00, 0.00, 0.00, 'Respawn Entertainment', 'Electronic Arts', '2020-11-04', '多人,免费开玩', NULL, NULL, NULL, 1, 4, 1, 0, 0, '2026-04-01 22:45:48', '2026-04-19 22:02:58', NULL);
-INSERT INTO `game` VALUES (17, 'Resident Evil Requiem', '予生者恐惧。给尸者安魂。\n\n《Resident Evil Requiem》是开创生存恐怖新纪元的系列最新作。与FBI分析员格蕾丝一同体验令人战栗的恐惧，并与资深特工里昂一同感受战胜死亡的畅快感。双角色的游戏体验，以及围绕二人纵横交错的剧情，绝对能直击玩家的灵魂。', '予生者恐惧。给尸者安魂。 《Resident Evil Requiem》标志着“Resident Evil”系列和生存恐怖游戏步入新纪元。“安魂曲”将会撼动玩家的精神深处。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/6903f617-b5f1-4a2e-a520-1a15f9c3b0df.jpg', '', 348.00, 0.00, 348.00, 'Capcom', 'Capcom', '2026-02-26', '冒险,动作,角色扮演', '', NULL, '', 1, 15, 2, 1, 0, '2026-04-01 23:08:36', '2026-04-21 22:31:08', NULL);
-INSERT INTO `game` VALUES (18, 'Pragmata', '“不管你去哪儿，我都会陪着你。”\n\n《Pragmata》是卡普空推出的一款独特的科幻动作冒险游戏。\n玩家将跟随命运多舛的调查队队员“休”和机器人小女孩“戴安娜”，在被失控AI掌管的\n月球设施内寻找返回地球的方法。\n■一场突如其来的事故，一对不同寻常的搭档\n\n人类在遥远的月球研究设施中，发现了一种新矿石。这种矿石经过精炼后可以用于3D打印\n机，再现出任何物体。\n\n在这座设施突然失联后，休跟随小队被派去调查……却遇到了一场灾难。\n\n独自徘徊在复杂设施中的人形机器人“戴安娜”救了休。二人必须合作才能穿过设施，找到\n离开月球的方法，但整座设施却被想阻止他们的AI所控制。', '《Pragmata》是卡普空推出的一款独特的科幻动作冒险游戏。 玩家将跟随命运多舛的调查队队员“休”和机器人小女孩“戴安娜”，在被失控AI掌管的月球设施内寻找返回地球的方法。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/2a293b94-6753-4b71-9153-a6ef8fcdc30c.jpg', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/17316475-cbf5-4091-ba44-76b1b862bc95.jpg', 248.00, 0.00, 248.00, 'CAPCOM', 'CAPCOM', '2026-04-16', '动作,冒险,角色扮演', '', NULL, '', 1, 5, 3, 1, 1, '2026-04-19 21:45:36', '2026-04-23 00:00:23', NULL);
-INSERT INTO `game` VALUES (19, 'PUBG: BATTLEGROUNDS', '降落， 纵身跃入不断扩展和变化的地图，迎接全新挑战。你会冒着物资匮乏的风险选择较为安全的边缘地带，还是直接空降人潮密集的城区，一落地就展开激战？每一次降落都是全新的挑战，如何利用手头资源生存，全凭你的战术与战略。 \n搜集， 独特武器、丰富物资与特殊道具助你智胜对手，如何武装自己，由你决定！为你钟爱的武器搭配强力配件，或为了稀有战利品冒险争夺众人觊觎的补给箱，扩大自己在战场上的优势。 \n求生，搜集物资只是生存的第一步。随着安全区不断缩小，遭遇其他生存者的几率也大幅上升。一旦阵亡，就彻底出局。只有比对手更聪明、更迅速，或枪法更胜一筹，才能存活到最后，成为最终吃鸡的赢家。多样玩法， \n\n在团队淘汰竞赛中热身，尝试街机模式的多变玩法，体验全新赛季活动，或创建自定义大厅，和好友来一场特别的较量！ \n独特战局 ，每一场战斗都独一无二，你的每次降落、搜集和求生的决策都至关重要。空降战场后，你的每一个选择都可能关乎胜负；战斗到底，力争成为最后的生存者。 \n可自由选择购买的游戏内商品已上架。', '《PUBG》，这款引领战术竞技热潮、紧张刺激的胜者为王射击游戏，现已开放免费游玩！在丰富多变的地图中降落，搜寻独特武器和物资，在不断缩小的安全区中求生，每个转角都可能是你的末路。', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/37410248-4e1d-4d1f-bc04-befbc9a2a691.jpg', '', 0.00, 0.00, 0.00, ' PUBG Corporation', ' KRAFTON, Inc.', '2017-12-20', '多人,免费开玩', '', NULL, '', 1, 2, 1, 0, 1, '2026-04-19 22:19:42', '2026-04-21 22:34:24', NULL);
 
 -- ----------------------------
 -- Table structure for game_category
@@ -177,14 +157,6 @@ CREATE TABLE `game_review`  (
   CONSTRAINT `fk_review_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '游戏评价表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of game_review
--- ----------------------------
-INSERT INTO `game_review` VALUES (1, 2, 17, 1, '好玩', 1, '2026-04-19 21:53:00', '2026-04-19 21:53:00', NULL, NULL);
-INSERT INTO `game_review` VALUES (2, 3, 18, 1, '强强', 1, '2026-04-19 21:57:32', '2026-04-19 21:57:32', NULL, NULL);
-INSERT INTO `game_review` VALUES (3, 2, 18, 0, '流程太短', 1, '2026-04-19 21:58:23', '2026-04-19 21:58:23', NULL, NULL);
-INSERT INTO `game_review` VALUES (4, 1, 19, 0, '挂壁多', 1, '2026-04-19 22:53:12', '2026-04-19 22:53:12', NULL, NULL);
-INSERT INTO `game_review` VALUES (5, 1, 6, 1, '夯', 1, '2026-04-19 22:53:25', '2026-04-19 22:53:25', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for hot_sale
@@ -205,19 +177,6 @@ CREATE TABLE `hot_sale`  (
   CONSTRAINT `fk_hot_sale_game` FOREIGN KEY (`game_id`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '热销榜表，仅保留当前前十' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of hot_sale
--- ----------------------------
-INSERT INTO `hot_sale` VALUES (4, '艾尔登法环', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/9d4856db-45cb-4c33-bc97-e063a34a740e.jpg', 3, 298.00, 1, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (5, '赛博朋克 2077', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/297cf394-edf0-4342-bdd4-da00ab4082e3.jpg', 3, 104.30, 2, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (6, '星露谷物语', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/f50873bb-704d-4daa-96ee-b3d123329692.jpg', 1, 24.00, 5, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (8, '只狼：影逝二度', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/9c435e21-6736-4d40-abf3-8dc7163867eb.jpg', 1, 138.60, 6, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (9, '巫师 3：狂猎', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/83450d3e-31d0-47bb-a828-1c3c5b02e162.jpg', 1, 39.50, 7, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (10, '荒野大镖客：救赎 2', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/375fdfe4-5abf-4d7e-87ae-2a2664e693a5.jpg', 0, 139.50, 10, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (15, 'Apex 英雄', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/357d8a08-728e-4793-a062-57fc94ccf143.jpg', 1, 0.00, 8, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (17, 'Resident Evil Requiem', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/6903f617-b5f1-4a2e-a520-1a15f9c3b0df.jpg', 2, 348.00, 4, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (18, 'Pragmata', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/2a293b94-6753-4b71-9153-a6ef8fcdc30c.jpg', 3, 248.00, 3, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
-INSERT INTO `hot_sale` VALUES (19, 'PUBG: BATTLEGROUNDS', 'https://hangyinengbishe.oss-cn-shanghai.aliyuncs.com/images/37410248-4e1d-4d1f-bc04-befbc9a2a691.jpg', 1, 0.00, 9, '2026-04-22 22:38:42', '2026-04-22 22:38:42');
 
 -- ----------------------------
 -- Table structure for order
@@ -245,25 +204,6 @@ CREATE TABLE `order`  (
   CONSTRAINT `fk_order_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '订单表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of order
--- ----------------------------
-INSERT INTO `order` VALUES (1, 'ORD17757379730820827668E', 1, 5, '赛博朋克 2077', 104.30, 1, 'CFXR-PBEB-S23Q-UHV7', '2026-04-09 20:32:53', '2026-04-09 20:32:53', '2026-04-09 20:32:53');
-INSERT INTO `order` VALUES (2, 'ORD1775737978193DFAFA308', 1, 5, '赛博朋克 2077', 104.30, 1, 'EEZK-4RHL-SHNA-HJNQ', '2026-04-09 20:32:58', '2026-04-09 20:32:58', '2026-04-09 20:32:58');
-INSERT INTO `order` VALUES (3, 'ORD1775738954772FE3B7F04', 1, 6, '星露谷物语', 24.00, 1, 'YWLP-29WH-Q6C4-VKGK', '2026-04-09 20:49:15', '2026-04-09 20:49:15', '2026-04-09 20:49:15');
-INSERT INTO `order` VALUES (4, 'ORD1775741701124CA903D19', 2, 5, '赛博朋克 2077', 104.30, 1, 'AKPQ-W78Y-MRGN-RMCQ', '2026-04-09 21:35:01', '2026-04-09 21:35:01', '2026-04-09 21:35:01');
-INSERT INTO `order` VALUES (5, 'ORD177574172126287BC2707', 2, 17, 'Resident Evil Requiem', 348.00, 1, 'PPCB-6283-W6DS-34LN', '2026-04-09 21:35:21', '2026-04-09 21:35:21', '2026-04-09 21:35:21');
-INSERT INTO `order` VALUES (6, 'ORD1776523311904CA3F4A37', 2, 9, '巫师 3：狂猎', 39.50, 1, 'T7E3-RXKQ-EAEQ-Q3ED', '2026-04-18 22:41:52', '2026-04-18 22:41:52', '2026-04-18 22:41:52');
-INSERT INTO `order` VALUES (7, 'ORD17765248354635524EF25', 2, 15, 'Apex 英雄', 0.00, 1, 'FTHJ-W2YJ-YYM2-QU3S', '2026-04-18 23:07:15', '2026-04-18 23:07:15', '2026-04-18 23:07:15');
-INSERT INTO `order` VALUES (8, 'ORD1776607022564DAB7643A', 3, 18, 'Pragmata', 248.00, 1, 'C7VV-V5W7-K8KX-B88C', '2026-04-19 21:57:03', '2026-04-19 21:57:03', '2026-04-19 21:57:03');
-INSERT INTO `order` VALUES (9, 'ORD177660706149970F62064', 3, 17, 'Resident Evil Requiem', 348.00, 1, 'XD99-CAUK-26ZQ-E764', '2026-04-19 21:57:41', '2026-04-19 21:57:42', '2026-04-19 21:57:41');
-INSERT INTO `order` VALUES (10, 'ORD1776607085407F17A104E', 2, 18, 'Pragmata', 248.00, 1, 'FXTJ-CAF7-4YSS-2DZ4', '2026-04-19 21:58:05', '2026-04-19 21:58:05', '2026-04-19 21:58:05');
-INSERT INTO `order` VALUES (11, 'ORD1776608534039711046EE', 1, 19, 'PUBG: BATTLEGROUNDS', 0.00, 1, 'XH3N-9J87-9YKN-HGKY', '2026-04-19 22:22:14', '2026-04-19 22:22:14', '2026-04-19 22:22:14');
-INSERT INTO `order` VALUES (12, 'ORD177660855551972951E5B', 1, 18, 'Pragmata', 248.00, 1, '68F5-53F8-YAA6-JJF2', '2026-04-19 22:22:36', '2026-04-19 22:22:36', '2026-04-19 22:22:36');
-INSERT INTO `order` VALUES (13, 'ORD1776616186539A66C0F16', 1, 4, '艾尔登法环', 298.00, 1, 'B86F-XG6P-GYWM-TXZ7', '2026-04-20 00:29:47', '2026-04-20 00:29:47', '2026-04-20 00:29:47');
-INSERT INTO `order` VALUES (14, 'ORD17766161931646BF1B3FD', 1, 4, '艾尔登法环', 298.00, 1, 'ZSHY-YWQC-RG52-5KH7', '2026-04-20 00:29:53', '2026-04-20 00:29:53', '2026-04-20 00:29:53');
-INSERT INTO `order` VALUES (15, 'ORD1776616695076225F5BF6', 1, 8, '只狼：影逝二度', 138.60, 1, '2TET-GNJK-C5Y2-H9YX', '2026-04-20 00:38:15', '2026-04-20 00:38:15', '2026-04-20 00:38:15');
-INSERT INTO `order` VALUES (16, 'ORD17768687221582E411A9B', 3, 4, '艾尔登法环', 298.00, 1, 'TRHR-S6LJ-F7XE-4YJJ', '2026-04-22 22:38:42', '2026-04-22 22:38:42', '2026-04-22 22:38:42');
 
 -- ----------------------------
 -- Table structure for shopping_cart
@@ -310,12 +250,6 @@ CREATE TABLE `user`  (
   INDEX `idx_email`(`email`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user
--- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', '$2a$10$ZJbWvsdSUN.yPCjj7Qbiw.j4cKwGcWVLM8IAbHIUA9FcL1K4cBFIC', 'admin@steammall.com', NULL, 'ADMIN', 1, '2026-03-21 01:47:56', '2026-03-21 02:14:13');
-INSERT INTO `user` VALUES (2, 'h1n', '$2a$10$DlrQ3poBRMGNkr91iDvssOIkoQigD/Skw2pC7qXYAF0apRp8Q7Brm', '', NULL, 'USER', 1, '2026-03-21 02:04:19', '2026-03-21 02:04:19');
-INSERT INTO `user` VALUES (3, 'h2n', '$2a$10$ZZhbQFJGmmIG/i.h8n8MIOCeMrHYh8S04a58QMZpAsXYuUbIMQi76', '123@123.com', NULL, 'USER', 1, '2026-04-01 22:32:44', '2026-04-01 22:32:44');
 
 -- ----------------------------
 -- Table structure for user_game
@@ -340,20 +274,5 @@ CREATE TABLE `user_game`  (
   CONSTRAINT `fk_user_game_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 13 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci COMMENT = '用户游戏库表' ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user_game
--- ----------------------------
-INSERT INTO `user_game` VALUES (1, 1, 5, 'EEZK-4RHL-SHNA-HJNQ', 1, '2026-04-09 20:33:04', 0, '2026-04-09 20:33:04');
-INSERT INTO `user_game` VALUES (2, 2, 5, 'AKPQ-W78Y-MRGN-RMCQ', 1, '2026-04-09 21:35:10', 0, '2026-04-09 21:35:10');
-INSERT INTO `user_game` VALUES (3, 2, 17, 'PPCB-6283-W6DS-34LN', 1, '2026-04-09 21:35:55', 0, '2026-04-09 21:35:55');
-INSERT INTO `user_game` VALUES (4, 1, 6, 'YWLP-29WH-Q6C4-VKGK', 1, '2026-04-18 22:33:43', 0, '2026-04-18 22:33:43');
-INSERT INTO `user_game` VALUES (5, 3, 18, 'C7VV-V5W7-K8KX-B88C', 1, '2026-04-19 21:57:10', 0, '2026-04-19 21:57:10');
-INSERT INTO `user_game` VALUES (6, 3, 17, 'XD99-CAUK-26ZQ-E764', 1, '2026-04-19 21:57:51', 0, '2026-04-19 21:57:51');
-INSERT INTO `user_game` VALUES (7, 2, 18, 'FXTJ-CAF7-4YSS-2DZ4', 1, '2026-04-19 21:58:13', 0, '2026-04-19 21:58:13');
-INSERT INTO `user_game` VALUES (8, 1, 19, 'XH3N-9J87-9YKN-HGKY', 1, '2026-04-19 22:22:22', 0, '2026-04-19 22:22:22');
-INSERT INTO `user_game` VALUES (9, 1, 18, '68F5-53F8-YAA6-JJF2', 1, '2026-04-19 22:22:43', 0, '2026-04-19 22:22:43');
-INSERT INTO `user_game` VALUES (10, 1, 4, 'ZSHY-YWQC-RG52-5KH7', 1, '2026-04-20 00:37:47', 0, '2026-04-20 00:37:47');
-INSERT INTO `user_game` VALUES (11, 1, 8, '2TET-GNJK-C5Y2-H9YX', 1, '2026-04-21 22:36:22', 0, '2026-04-21 22:36:22');
-INSERT INTO `user_game` VALUES (12, 3, 4, 'TRHR-S6LJ-F7XE-4YJJ', 1, '2026-04-22 22:39:01', 0, '2026-04-22 22:39:01');
 
 SET FOREIGN_KEY_CHECKS = 1;
